b2d570a9c257be9ba0195dc5b494f819e5b42415
Aleksey Lapshin <lapnem@yandex.ru>
2023-03-16 23:04:53 +0300

    leetcode task arrays complite

---
+++ courses/leetcode/Arrays101/01-RemoveDuplicates_from_SortedArray.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+def removeDuplicates(nums: List[int]) -> int:
+    # nums = list(set(nums))
+    index = 0
+    while True:
+        try:
+            next = nums[index+1]
+            print(f"Шаг {index}")
+            print(nums[index], nums[next])
+            if next == nums[index]:
+                nums.pop(index+1)
+                continue
+            index += 1
+        except:
+            break
+    return len(nums)
+
+
+nums1 = [1, 1, 2]
+nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+
+answer1 = removeDuplicates(nums1)
+answer2 = removeDuplicates(nums2)
+
+print(f"{answer1=}")
+print(f"{answer2=}")
